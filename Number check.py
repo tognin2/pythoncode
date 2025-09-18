572555acc2fe2f4d1e2851717b798fdcd98a70d3
tognin2 <gabriel.tognin@hotmail.com>
2025-09-18 17:01:13 -0300

    Create Number check.py

---
+++ Number check.py
@@ -0,0 +1,8 @@
+print("Welcome to the Number Check.")
+number_check = int(input("Text here a number to check if it's a Odd or Even: "))
+if number_check % 2 == 0:
+    print("Your number is a even number")
+else:
+    print("Your number is a odd number")
+
+
