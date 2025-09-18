66198417572da9f025784fbf0bb4603ac3df0ae4
tognin2 <gabriel.tognin@hotmail.com>
2025-09-18 16:44:40 -0300

    Add files via upload

---
+++ practice3.py
@@ -0,0 +1,5 @@
+print("Welcome to the salary calculator based on how much you gain/hour")
+gain_per_hour = int(input("How much you gain per hour? "))
+gain_per_month = gain_per_hour * 200
+print(f"If you gain {gain_per_hour} per hour, in a month you will earn {gain_per_month} reais ")
+
